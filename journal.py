abf6eea5148dc878bedbef91acec4ac324b53879
Georgiy <128026153+GVA-ranepa@users.noreply.github.com>
2023-07-21 11:46:17 +0300

    Update journal.py

--- journal.py
+++ journal.py
@@ -2,11 +2,6 @@ import re
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-#import os
-#import pandas as pd
-#import time
-#from bs4 import BeautifulSoup
 
 # Запускаем драйвер, открываем веб-страницу Журналы
 browser = webdriver.Firefox()
